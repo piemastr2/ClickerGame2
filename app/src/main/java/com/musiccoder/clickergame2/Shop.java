a30ca2e012d6ccba9b4073c199dab8e4acb590e7
Brian Lee <blee12@uci.edu>
2019-02-17 00:01:46 -0800

    Lots of UI changes.

--- app/src/main/java/com/musiccoder/clickergame2/Shop.java
+++ app/src/main/java/com/musiccoder/clickergame2/Shop.java
@@ -1,8 +1,11 @@
 package com.musiccoder.clickergame2;
 
+import android.content.Intent;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Shop extends AppCompatActivity {
 
@@ -15,5 +18,15 @@ public class Shop extends AppCompatActivity {
         if (actionBar != null) {
             actionBar.hide();
         }
+
+        Button btn_buyPlants = (Button)findViewById(R.id.shop_tree);
+
+        btn_buyPlants.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(Shop.this, BuyPlantPop.class));
+            }
+        });
     }
 }
+
